d58c54e57cc5532885d583f93676a0313d59f51b
Raghav Ingole <112391405+Raghav211205@users.noreply.github.com>
2026-04-27 10:28:35 +0530

    Update sol5.py

--- project_euler/problem_001/sol5.py
+++ project_euler/problem_001/sol5.py
@@ -8,13 +8,10 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
-
-
 def solution(n: int = 1000) -> int:
     """
     Returns the sum of all the multiples of 3 or 5 below n.
     A straightforward pythonic solution using list comprehension.
-
     >>> solution(3)
     0
     >>> solution(4)
@@ -24,9 +21,6 @@ def solution(n: int = 1000) -> int:
     >>> solution(600)
     83700
     """
-
     return sum(i for i in range(n) if i % 3 == 0 or i % 5 == 0)
-
-
 if __name__ == "__main__":
     print(f"{solution() = }")
